f2a078340c1a3f990bd8b651d46fbb7a4572b258
smnshzh <smnshzh@gmail.com>
2020-08-21 23:12:16 +0430

    orders added

--- order/models.py
+++ order/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from warehouse.models import *
 class Order(models.Model):
 
@@ -7,8 +6,13 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
 
-    code     = models.ForeignKey(Order,on_delete=models.CASCADE)
-    product  = models.ForeignKey(Product,on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    price    = models.PositiveIntegerField()
-    total_price = quantity*price
+    code = models.ForeignKey (Order, on_delete=models.CASCADE)
+    product = models.ForeignKey (Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField ( )
+    price = models.PositiveIntegerField ( )
+
+    def total_price(self):
+        total_price = self.quantity * self.price
+        return total_price
+
+    total_price = property (total_price)
